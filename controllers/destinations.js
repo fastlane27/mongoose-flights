6b9d8680fc961b7fa377e70cf204763ca85f1e44
David Lane <fastlane27@gmail.com>
2020-02-25 17:30:10 -0600

    Adjust destination controller

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -8,7 +8,6 @@ function create(req, res) {
     Flight.findById(req.params.id, function(err, flight) {
         flight.destinations.push(req.body);
         flight.save(function(err) {
-            if (err) return res.redirect(`/flights/${req.params.id}`);
             res.redirect(`/flights/${req.params.id}`);
         });
     });
